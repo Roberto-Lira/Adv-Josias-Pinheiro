e0b0d60b682c596549bd92cfe61e6ade903bcfbd
unknown <robertoengenharia.ufsm@gmail.com>
2022-01-22 20:53:35 -0300

    primeiro commit

---
+++ main.js
@@ -0,0 +1,23 @@
+
+var caixas = document.querySelectorAll(".caixa");
+
+//terei então um array
+
+var botao = document.querySelector(".caixa");
+
+botao.addEventListener("click", function(event){
+
+	//var el = event.target || event.srcElement;
+
+	//var id = el.id;
+
+	for(var i = 0; i < caixas.length; i++){
+
+	var caixa = caixas[i];
+
+	var conteudo = ...........
+	if(caixa.id = )
+	}
+
+})
+
